77be9dae23239f9d956f48a967845d0815ab8873
Maksim Shcherbakov <i@max87.ru>
2024-03-02 12:27:58 +0300

    Fun fix added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@
 fun main() {
     var sumDay = 0
     var sumMonth = 0
-    var type = "MIR"
+    var type = "MASTERCARD"
     println("Введите сумму перевода: ")
     val amount = readln().toInt()
     transfer(type, sumDay, sumMonth, amount)
@@ -14,24 +14,25 @@ fun limitCheck(amount: Int, sumDay: Int, sumMonth: Int): Boolean {
     if (amount + sumMonth > limitMonth) return false
     return true
 }
-fun mir(amount: Int) {
+fun mir(amount: Int): Unit {
     println("Сумма перевода: $amount. Размер комиссии: 0 руб.")
 }
-fun visa(amount: Int) {
+fun visa(amount: Int): Unit {
     val comission = 0.75
     val minComission = 35
     val result = amount * comission / 100
     val totalComission = if (result > minComission) result else minComission
     println("Сумма перевода: $amount. Размер комиссии за перевод: $totalComission руб.")
 }
-fun mastercard(amount: Int, sumMonth: Int) {
+fun mastercard(amount: Int, sumMonth: Int): Unit {
     val monthLimit = 75_000
     val comission = 0.006
     var result = 0
-    if (amount+sumMonth > monthLimit) result = (amount*comission).toInt() + 20
+    if (sumMonth >= monthLimit) result = (amount*comission).toInt() + 20
+    if (amount+sumMonth > monthLimit && sumMonth < monthLimit) result = ((sumMonth + amount - monthLimit) *comission).toInt() + 20
     println("Сумма перевода: $amount. Размер комиссии за перевод: $result руб.")
 }
-fun transfer(type: String, sumDay: Int, sumMonth: Int, amount: Int) {
+fun transfer(type: String, sumDay: Int, sumMonth: Int, amount: Int): Unit {
     if (!limitCheck(amount, sumDay, sumMonth))
         println("Перевод невозможен, превышены лимиты!")
     when(type) {
